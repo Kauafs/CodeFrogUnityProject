b74510c090a6826a92ea4ae3740571e64a305e92
Kauafs <felipekauads@gmail.com>
2025-04-10 02:01:27 -0300

    Atualização da cena e inclusão de novos assets e scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float forceMultiplier = 3f;
+    public float maximumVelocity = 3f;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal"); //tecla direita ou esquerda, ou tecla a ou d
+        
+        if(rb.velocity.magnitude <= maximumVelocity)
+        {
+            rb.AddForce(new Vector3(horizontalInput * forceMultiplier, 0, 0));
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("PerigoTag"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
